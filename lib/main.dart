2b6e64e48f367bde81366ff59c00284795b283d1
Romário Lima <devromariolima@gmail.com>
2024-05-29 18:07:58 -0300

    setState: Criação do gerenciamento de estado

--- lib/main.dart
+++ lib/main.dart
@@ -15,9 +15,36 @@ class AppWidget extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       theme: ThemeData(primarySwatch: Colors.red),
-      home: Container(
-        child: const Center(child: Text('Romário Lima')),
-      ),
+      home: const HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() {
+    return HomePageState();
+  }
+}
+
+class HomePageState extends State<HomePage> {
+  int counter = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Center(
+          child: GestureDetector(
+        child: Text('Contador: $counter'),
+        onTap: () {
+          setState(() {
+            counter++;
+          });
+          print(counter);
+        },
+      )),
     );
   }
 }
