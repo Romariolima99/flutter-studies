aebca8d2f8d758c51b494f3129e03f579db80fbe
Romário Lima <devromariolima@gmail.com>
2024-05-29 17:25:56 -0300

    Criação do Widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,24 @@
 import 'package:flutter/material.dart';
 
 main() {
-  runApp(Container(
-    child: const Text(
-      'Hello World',
-      textDirection: TextDirection.ltr,
-      style: TextStyle(color: Colors.blue),
-    ),
+  runApp(const AppWidget(
+    title: 'Romário Lima',
   ));
+}
+
+class AppWidget extends StatelessWidget {
+  final String title;
+
+  const AppWidget({super.key, required this.title});
+
+ @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Text(
+        title,
+        textDirection: TextDirection.ltr,
+        style: TextStyle(color: Colors.blue, fontSize: 50.0),
+      ),
+    );
+  }
 }
