7adfb82de3c4b78a5661c64955f2858c89752f28
Romário Lima <devromariolima@gmail.com>
2024-05-29 17:28:03 -0300

    Widget: Centrralização do widget

--- lib/main.dart
+++ lib/main.dart
@@ -14,11 +14,12 @@ class AppWidget extends StatelessWidget {
  @override
   Widget build(BuildContext context) {
     return Container(
+      child: Center(
       child: Text(
         title,
         textDirection: TextDirection.ltr,
         style: TextStyle(color: Colors.blue, fontSize: 50.0),
       ),
-    );
+    ));
   }
 }
